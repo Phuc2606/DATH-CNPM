c5ff848f9fc83e3cf7a3560810abfb6d8728f79a
Phuc2606 <phuc26062005@gmail.com>
2025-10-18 16:50:55 +0700

    Setup UI

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Homepage from './pages/Homepage/Homepage'
+const App = () => {
+  return (
+    <div className='App'>
+      <Routes>
+        <Route path = '/' element={<Homepage/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div>
+      1
+    </div>
+  )
+}
+
+export default Header
